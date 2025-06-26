13ae2ec1381f430144a59f0ba4810b28956a0a8a
Gandham Geetha Sriya <geethagandham688@gmail.com>
2025-06-26 18:44:48 +0530

    Delete database directory

--- database/schema.sql
+++
@@ -1,8 +0,0 @@
--- This is a sample SQL schema if you’re using SQLite/MySQL
-
-CREATE TABLE property (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    price INTEGER NOT NULL,
-    location TEXT NOT NULL
-);
